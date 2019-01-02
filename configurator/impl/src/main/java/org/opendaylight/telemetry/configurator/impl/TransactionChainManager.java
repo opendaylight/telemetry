0c950c4943b0c729e75299102400ddc59e40c849
lijiansong77 <lijiansong@chinatelecom.cn>
2019-01-02 17:52:44 +0800

    add txlock to manager of transaction to prevent conflict.
    Change-Id: Ib1231b9d17c918c2b50d7a21c78daf306b043c23
    Signed-off-by: lijiansong77 <lijiansong@chinatelecom.cn>

--- configurator/impl/src/main/java/org/opendaylight/telemetry/configurator/impl/TransactionChainManager.java
+++ configurator/impl/src/main/java/org/opendaylight/telemetry/configurator/impl/TransactionChainManager.java
@@ -8,7 +8,6 @@
 package org.opendaylight.telemetry.configurator.impl;
 
 
-import java.util.Optional;
 import javax.annotation.Nonnull;
 import org.opendaylight.controller.md.sal.binding.api.BindingTransactionChain;
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
@@ -16,35 +15,49 @@ import org.opendaylight.controller.md.sal.binding.api.WriteTransaction;
 import org.opendaylight.controller.md.sal.common.api.data.AsyncTransaction;
 import org.opendaylight.controller.md.sal.common.api.data.TransactionChain;
 import org.opendaylight.controller.md.sal.common.api.data.TransactionChainListener;
-import javax.annotation.Nonnull;
-import org.opendaylight.mdsal.binding.api.Transaction;
+import javax.annotation.concurrent.GuardedBy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
  * @author: li.jiansong
  **/
-public class TransactionChainManager implements TransactionChainListener{
+public class TransactionChainManager implements TransactionChainListener {
     private static final Logger LOG = LoggerFactory.getLogger(TransactionChainManager.class);
     private static final String CANNOT_WRITE_INTO_TRANSACTION = "Cannot write into transaction.";
-
     private final Object txLock = new Object();
     private final DataBroker dataBroker;
-    private BindingTransactionChain txChainFactory;
-    private WriteTransaction wTx;
     private final String nodeId;
+
+    @GuardedBy("txLock")
+    private BindingTransactionChain txChainFactory;
+    @GuardedBy("txLock")
+    private WriteTransaction wTx;
 
     TransactionChainManager(@Nonnull final DataBroker dataBroker,
                             @Nonnull final String nodeIp) {
         this.dataBroker = dataBroker;
         this.nodeId = nodeIp;
+        synchronized (txLock) {
+            createTxChain();
+        }
+    }
+
+    void activateTransactionManager() {
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("activateTransactionManager for node {}", this.nodeId);
+        }
+        synchronized (txLock) {
+            createTxChain();
+        }
     }
 
     @Override
     public void onTransactionChainSuccessful(@Nonnull TransactionChain chain) {
-
-        // NOOP
+        LOG.debug("transtion chain successful.");
     }
+
+    @GuardedBy("txLock")
     private void createTxChain() {
         txChainFactory = dataBroker.createTransactionChain(TransactionChainManager.this);
     }
