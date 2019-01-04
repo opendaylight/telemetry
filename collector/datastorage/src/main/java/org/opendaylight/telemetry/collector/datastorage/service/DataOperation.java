a8ef8f1dc85e130f45494eb7f51b5af098202ef5
lijiansong77 <lijiansong@chinatelecom.cn>
2019-01-04 16:21:13 +0800

    add api of datastorege service for other module querying collector data.
    Change-Id: I6e8e681219774732d1ba3834adc07e2537c35377
    Signed-off-by: lijiansong77 <lijiansong@chinatelecom.cn>

---
+++ collector/datastorage/src/main/java/org/opendaylight/telemetry/collector/datastorage/service/DataOperation.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright © 2017 CTCC. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.telemetry.collector.datastorage.service;
+
+/**
+ * @author: li.jiansong
+ **/
+public interface DataOperation {
+    void queryTelemetryData();
+}
---
+++ collector/datastorage/src/main/java/org/opendaylight/telemetry/collector/datastorage/service/DataOperationService.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright © 2017 CTCC. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.telemetry.collector.datastorage.service;
+
+/**
+ * @author: li.jiansong
+ **/
+public class DataOperationService implements DataOperation{
+    public void init() {
+
+    }
+
+    @Override
+    public void queryTelemetryData() {
+
+    }
+
+    public void close() {
+
+    }
+
+}
